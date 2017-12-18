a448d3e37d44050c20e2e8d756367bdd6c02f5ec
Rakibul Islam <ricky@solaritsolutions.com>
2017-12-17 18:00:40 -0600

    create unit test files, add device deployment

---
+++ YelpSampleTests/RestaurantTests.swift
@@ -0,0 +1,39 @@
+//
+//  RestaurantTests.swift
+//  YelpSampleTests
+//
+//  Created by Rakibul Islam on 12/17/17.
+//  Copyright © 2017 Rakibul Islam. All rights reserved.
+//
+
+import XCTest
+@testable import YelpSample
+
+class RestaurantTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testInitRestaurant_withDictionary_shouldCreateRestaurant() {
+        // This is an example of a functional test case.
+        let dict = ["name":"Restaurant Name",
+                    "image_url":"www.image.com",
+                    "rating_img_url_large":"www.starimage.com",
+                    "snippet_text": "Snippet"]
+        guard let restaurant = Restaurant(dict: dict) else {
+            XCTFail("Restaurant not created!")
+            return
+        }
+        XCTAssertEqual(restaurant.name, "Restaurant Name")
+        XCTAssertEqual(restaurant.photoUrl, "www.image.com")
+        XCTAssertEqual(restaurant.ratingImageUrl, "www.starimage.com")
+        XCTAssertEqual(restaurant.latestReview, "Snippet")
+    }
+}
