2fc15b986ccc8d873e66fe8ef4c33bb740c7e1b9
Rakibul Islam <ricky@solaritsolutions.com>
2020-04-26 13:21:34 -0400

    Remove CocoaPods
    Remove CocoaPods dependencies from project

--- Podfile
+++
@@ -1,5 +0,0 @@
-platform :ios, '11.0'
-use_frameworks!
-target 'YelpSample' do
-  pod 'OAuthSwift', '~> 1.2.0'
-end
--- Pods/Target Support Files/Pods-YelpSample/Pods-YelpSample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YelpSample : NSObject
-@end
-@implementation PodsDummy_Pods_YelpSample
-@end
